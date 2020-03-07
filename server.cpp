bc415fc629928e91b64f16647bae3ff4b0bd6e62
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-07 12:39:07 -0700

    Here we go

--- server.cpp
+++ server.cpp
@@ -6,6 +6,7 @@
 // --------------------------------------------------------------------------------
 
 #include "digraph.cpp"
+#include "wdigraph.h"
 #include <iostream>
 #include <fstream>
 #include <queue>
@@ -19,6 +20,7 @@ struct Point {
     long long lon; // longitude of the point
 };
 
+
 long long manhattan(const Point& pt1, const Point& pt2) {
     long long dist = 0;
     long long x = abs(pt1.lat - pt2.lat);
@@ -26,7 +28,6 @@ long long manhattan(const Point& pt1, const Point& pt2) {
     dist = x+y;
     return dist;
 }
-
 //Taking the latitude and longitude finds the nearest vertex
 int findVertex(long long lat, long long lon, unordered_map<int, Point>& vertice){
     long long oldLon = 10000;
@@ -48,87 +49,87 @@ int findVertex(long long lat, long long lon, unordered_map<int, Point>& vertice)
 }
 
 void readGraph(string filename, WDigraph& graph, unordered_map<int, Point>& points) {
-	// Initializing of variables
-	
-	string line;
-	fstream file;
-	file.open(filename);
+    // Initializing of variables
+    
+    string line;
+    fstream file;
+    file.open(filename);
 
 
-	Point pt1;
-	
+    Point pt1;
+    
 
-	// Reading the file line by line until there are no lines left
-	
-	while(getline(file, line)){
-		
-		string str2 = line.substr(0,1);
+    // Reading the file line by line until there are no lines left
+    
+    while(getline(file, line)){
+        
+        string str2 = line.substr(0,1);
 
-		// If the first char in the line is V then a vertex will be added
-		if(str2 == "V"){
-			
-			int start = line.find(',');
-			int end = line.find(',', start + 1);
+        // If the first char in the line is V then a vertex will be added
+        if(str2 == "V"){
+            
+            int start = line.find(',');
+            int end = line.find(',', start + 1);
 
-			string subS = line.substr(start+1, end-start-1);
+            string subS = line.substr(start+1, end-start-1);
 
-			start = end;
-			end = line.find(',', start+1);
-			//cout<<end<<endl;
-			//cout<<"_____"<<endl;
-			string lat = line.substr(start+1, end-start-1);
-			//cout<<"lat "<< lat<<endl;
+            start = end;
+            end = line.find(',', start+1);
+            //cout<<end<<endl;
+            //cout<<"_____"<<endl;
+            string lat = line.substr(start+1, end-start-1);
+            //cout<<"lat "<< lat<<endl;
 
-			start = end;
+            start = end;
 
-			//end = line.find('\n', start +1);
-			//cout<<end<<endl;
-			//cout<<"_____"<<endl;
-			string lon = line.substr(start+1, line.length()-1);
-			//cout<<"lon "<<lon<<endl;
+            //end = line.find('\n', start +1);
+            //cout<<end<<endl;
+            //cout<<"_____"<<endl;
+            string lon = line.substr(start+1, line.length()-1);
+            //cout<<"lon "<<lon<<endl;
 
-			int vertex = stoi(subS);
-			double latDoub = stod(lat);
-			double lonDoub = stod(lon);
-			//cout<<vertex<<endl;
+            int vertex = stoi(subS);
+            double latDoub = stod(lat);
+            double lonDoub = stod(lon);
+            //cout<<vertex<<endl;
 
-			long long newLon = static_cast<long long>(lonDoub*100000);
-			long long newLat = static_cast<long long>(latDoub*100000);
+            long long newLon = static_cast<long long>(lonDoub*100000);
+            long long newLat = static_cast<long long>(latDoub*100000);
 
-			pt1.lat = newLat;
-			pt1.lon = newLon;
-			//cout<<pt1.lat<< " " <<pt1.lon<<endl;
-			points[vertex] = pt1;
-			//cout<<"here11"<<endl;
-			graph.addVertex(vertex);
-			//break;
-			
-			
+            pt1.lat = newLat;
+            pt1.lon = newLon;
+            //cout<<pt1.lat<< " " <<pt1.lon<<endl;
+            points[vertex] = pt1;
+            //cout<<"here11"<<endl;
+            graph.addVertex(vertex);
+            //break;
+            
+            
 
-		// Else if the first char in the line is E then an edge will be added
-		} else if(str2 == "E"){
-			int startingPos1 = line.find(",", 2) - 2;
-			int startingPos2 = line.find(",", line.find(",", 2) + 1) - 3 - startingPos1;
+        // Else if the first char in the line is E then an edge will be added
+        } else if(str2 == "E"){
+            int startingPos1 = line.find(",", 2) - 2;
+            int startingPos2 = line.find(",", line.find(",", 2) + 1) - 3 - startingPos1;
 
-			string str3 = line.substr(2,startingPos1);
-			string str4 = line.substr(line.find(",", 2) + 1,startingPos2);
-			//cout<<str3<<" "<<str4<<endl;
-			int vertex1 = stoi(str3);
+            string str3 = line.substr(2,startingPos1);
+            string str4 = line.substr(line.find(",", 2) + 1,startingPos2);
+            //cout<<str3<<" "<<str4<<endl;
+            int vertex1 = stoi(str3);
 
-			int vertex2 = stoi(str4);
+            int vertex2 = stoi(str4);
 
-			long long cost = manhattan(points[vertex1], points[vertex2]);
+            long long cost = manhattan(points[vertex1], points[vertex2]);
 
-			graph.addEdge(vertex1, vertex2, cost);
-			//graph.addEdge(vertex2, vertex1, cost);
-			
-		}
-	}
-	file.close();
-	
+            graph.addEdge(vertex1, vertex2, cost);
+            //graph.addEdge(vertex2, vertex1, cost);
+            
+        }
+    }
+    file.close();
+    
 }
 
-void server(char inputFile[], char outputFile[]){
+void server(char inputFile[], char outputFile[], unordered_map<int, Point>& nodes){
     string inFile = inputFile;
     string outFile = outputFile;
     ifstream input;
@@ -150,15 +151,21 @@ void server(char inputFile[], char outputFile[]){
             long long lat2 = stoi(dataLine.substr(space3+1, space4-space3));
             int end = dataLine.find("\n", space4+1);
             long long lon2 = stoi(dataLine.substr(space4+1, end-space4));
-            cout << lat1 << " " << lon1 << " " << lat2 <<" " << lon2 << endl;
+            int vertex1 = findVertex(lat1, lon1, nodes);
+            int vertex2 = findVertex(lat2, lon2, nodes);
             break;
         }
     }
 }
 
+
 int main(int argc, char *argv[])
 {
-    char *file = argv[1];
+    unordered_map<int, Point> test; 
+    WDigraph graph;
+    char *inputFile = argv[1];
+    char *outputFile = argv[2];
+    readGraph("edmonton-roads-2.0.1.txt", graph, test);
 
     return 0;
 }
