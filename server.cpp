af2fdca7e480e9ac4593a01ec63be51a811b5625
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-07 09:01:00 -0700

    Create server.cpp

---
+++ server.cpp
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------
+// Names: Paige Lekach & Justin Mackenzie
+// CMPUT 275, Winter 2020
+//
+// Assignment #2 Route finder Part #1
+// --------------------------------------------------------------------------------
+
+#include "digraph.cpp"
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+    char *file = argv[1];
+
+    return 0;
+}
