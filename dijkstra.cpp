d7976a5afb47c4f54494b4f47f17d36f0ee016da
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-14 11:04:53 -0600

    THE ALOGOTITHm

---
+++ dijkstra.cpp
@@ -0,0 +1,24 @@
+#include "dijkstra.h"
+#include "heap.h"
+
+
+void dijkstra(const WDigraph& graph, int startVertex, unordered_map<int, PIL>& tree) {
+
+    BinaryHeap<PIL, long long> events;
+    events.insert(PIL(startVertex,startVertex), 0);
+    while(events.size() > 0){
+        HeapItem<PIL, long long> node = events.min();
+        events.popMin();
+        long long v = node.item.second;
+        if(tree.find(v) == tree.end()){
+            tree[v] = node.item;
+            for (auto iter = graph.neighbours(v); iter != graph.endIterator(v); iter++) {
+                long long w = *iter;
+                PIL next = PIL(v,w);
+                long long cost = graph.getCost(v,w);
+                events.insert(next,cost);
+            }
+        }
+
+    }
+}
