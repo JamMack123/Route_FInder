4579e0d10e4daeb55fd19d1e3c9e143901377189
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-14 13:24:45 -0600

    new

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,22 +1,24 @@
 #include "dijkstra.h"
 #include "heap.h"
 
-
-void dijkstra(const WDigraph& graph, int startVertex, unordered_map<int, PIL>& tree) {
-
+void dijkstra(const WDigraph &graph, int startVertex, unordered_map<int, PIL> &tree)
+{
     BinaryHeap<PIL, long long> events;
-    events.insert(PIL(startVertex,startVertex), 0);
-    while(events.size() > 0){
+    events.insert(PIL(startVertex, startVertex), 0);
+    while(events.size() > 0)
+    {
         HeapItem<PIL, long long> node = events.min();
         events.popMin();
         long long v = node.item.second;
-        if(tree.find(v) == tree.end()){
+        if(tree.find(v) == tree.end())
+        {
             tree[v] = node.item;
-            for (auto iter = graph.neighbours(v); iter != graph.endIterator(v); iter++) {
+            for (auto iter = graph.neighbours(v); iter != graph.endIterator(v); iter++)
+            {
                 long long w = *iter;
-                PIL next = PIL(v,w);
-                long long cost = node.key + graph.getCost(v,w);
-                events.insert(next,cost);
+                PIL next = PIL(v, w);
+                long long cost = node.key + graph.getCost(v, w);
+                events.insert(next, cost);
             }
         }
 
