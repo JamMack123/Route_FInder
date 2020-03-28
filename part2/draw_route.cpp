7cb5876576b970af983488a9ecdb4be12637107c
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-03-28 10:48:35 -0600

    Formatted

--- part2/draw_route.cpp
+++ part2/draw_route.cpp
@@ -9,18 +9,21 @@ extern shared_vars shared;
     Outputs: NONE
 */
 
-void draw_route() {
+void draw_route()
+{
 
     // Case where there are no waypoints
-    if(shared.num_waypoints == 0){
+    if(shared.num_waypoints == 0)
+    {
         status_message("Unable to find route");
         delay(1000);
     }
     // Iterating through each waypoint in the shared variable
-    for(int i = 1; i <shared.num_waypoints; i++){
+    for(int i = 1; i < shared.num_waypoints; i++)
+    {
 
         // Storing the two points to draw a line between
-        lon_lat_32 first = shared.waypoints[i-1];
+        lon_lat_32 first = shared.waypoints[i - 1];
         lon_lat_32 second = shared.waypoints[i];
 
         // Converting points to x/y
@@ -37,9 +40,9 @@ void draw_route() {
         y0 -= offset.y;
         x1 -= offset.x;
         y1 -= offset.y;
-        
+
         // Displaying route
-        shared.tft->drawLine(x0,y0,x1,y1,TFT_BLUE);
+        shared.tft->drawLine(x0, y0, x1, y1, TFT_BLUE);
 
 
 
